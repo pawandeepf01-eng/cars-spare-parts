a177765f1ce19af6e8438fa8dbfb977f0afa2b18
pawandeepf01-eng <pawandeep.f01@gmail.com>
2026-05-11 23:16:44 +0530

    fix route5

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://dhiman-motors.vercel.app/",
+      "https://dhiman-motors.vercel.app",
     ],
     credentials: true,
   })
