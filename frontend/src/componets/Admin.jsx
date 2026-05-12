91cf0dc5ecd3fad526a93c9300472b20d32dc1f6
pawandeepf01-eng <pawandeep.f01@gmail.com>
2026-05-12 10:25:24 +0530

    add

--- frontend/src/componets/Admin.jsx
+++ frontend/src/componets/Admin.jsx
@@ -26,7 +26,7 @@ function Admin() {
       formData.append("img", data.img[0]); // file input gives array
 
       // Send with axios
-      const res = await axios.post("https://cars-spare-parts.onrender.com/add", formData, {
+      const res = await axios.post("https://cars-spare-parts.onrender.com/api/add", formData, {
         withCredentials: true,
         headers: { "Content-Type": "multipart/form-data" },
       });
